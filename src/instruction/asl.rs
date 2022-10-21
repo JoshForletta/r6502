3c25cd035598108a6519440a20f42413b952f00a
Josh Forletta <joshforletta@gmail.com>
2022-10-21 08:39:57 -0700

    Implemented asl instruction

--- src/instruction/asl.rs
+++ src/instruction/asl.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -42,27 +43,150 @@ pub const ASL_ABSOLUTE_X: Instruction = Instruction {
 };
 
 pub fn asl(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    let target = (am.call)(cpu)?;
+    let mut data = target.clone() as u16;
+
+    data = data << 1;
+
+    let (carry, data) = ((data >> 8) as u8, data as u8);
+
+    *target = data;
+
+    cpu.ps.set(PS::C, carry != 0);
+    cpu.ps.set(PS::Z, data == 0);
+    cpu.ps.set(PS::N, (data & 0x80) != 0);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn asl_accumulator() {}
-    //
-    // #[test]
-    // fn asl_zero_page() {}
-    //
-    // #[test]
-    // fn asl_zero_page_x() {}
-    //
-    // #[test]
-    // fn asl_absolute() {}
-    //
-    // #[test]
-    // fn asl_absolute_x() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn asl_carry_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x0A],
+            initial_cpu_state: CpuState {
+                a: Some(0xA0),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::C),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_zero_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x0A],
+            initial_cpu_state: CpuState {
+                a: Some(0x80),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::Z | PS::C),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_negative_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x0A],
+            initial_cpu_state: CpuState {
+                a: Some(0x40),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::N),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_accumulator() {
+        let est = EmulationStateTest {
+            instructions: &[0x0A],
+            initial_cpu_state: CpuState {
+                a: Some(0xA0),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                a: Some(0x40),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_zero_page() {
+        let est = EmulationStateTest {
+            instructions: &[0x06, 0x02, 0xA0],
+            mem_tests: &[(0x02, 0x40)],
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_zero_page_x() {
+        let est = EmulationStateTest {
+            instructions: &[0x16, 0x01, 0xA0],
+            initial_cpu_state: CpuState {
+                x: Some(0x01),
+                ..Default::default()
+            },
+            mem_tests: &[(0x02, 0x40)],
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_absolute() {
+        let est = EmulationStateTest {
+            instructions: &[0x0E, 0x03, 0x00, 0xA0],
+            mem_tests: &[(0x03, 0x40)],
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn asl_absolute_x() {
+        let est = EmulationStateTest {
+            instructions: &[0x1E, 0x02, 0x00, 0xA0],
+            initial_cpu_state: CpuState {
+                x: Some(0x01),
+                ..Default::default()
+            },
+            mem_tests: &[(0x03, 0x40)],
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
