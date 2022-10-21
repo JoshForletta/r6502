b97a8968ee2096135f3efc42428005d473f0803c
Josh Forletta <joshforletta@gmail.com>
2022-10-21 09:09:11 -0700

    Implemented bcc instruction

--- src/instruction/bcc.rs
+++ src/instruction/bcc.rs
@@ -1,8 +1,9 @@
-use std::error::Error;
+use std::{error::Error, num::Wrapping};
 
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -14,15 +15,44 @@ pub const BCC_RELATIVE: Instruction = Instruction {
 };
 
 pub fn bcc(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    let offset = *(am.call)(cpu)? as i8;
+
+    if !cpu.ps.contains(PS::C) {
+        cpu.pc = (Wrapping(cpu.pc as i16) + Wrapping(Into::<i16>::into(offset))).0 as u16;
+    }
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn bcc_relative() {}
+    use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
+
+    #[test]
+    fn bcc_positive_offset() {
+        let est = EmulationStateTest {
+            instructions: &[0x90, 0x02],
+            test_cpu_state: CpuState {
+                pc: Some(0x04),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn bcc_negative_offset() {
+        let est = EmulationStateTest {
+            instructions: &[0x90, 0xFF],
+            test_cpu_state: CpuState {
+                pc: Some(0x01),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
