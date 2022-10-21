555c367a982b29b2d4de5578d5a4e3f2477ec7bf
Josh Forletta <joshforletta@gmail.com>
2022-10-21 10:12:12 -0700

    Implemented bne instruction

--- src/instruction/bne.rs
+++ src/instruction/bne.rs
@@ -1,8 +1,9 @@
-use std::error::Error;
+use std::{error::Error, num::Wrapping};
 
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -14,15 +15,65 @@ pub const BNE_RELATIVE: Instruction = Instruction {
 };
 
 pub fn bne(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    let offset = *(am.call)(cpu)? as i8;
+
+    if !cpu.ps.contains(PS::Z) {
+        cpu.pc = (Wrapping(cpu.pc as i16) + Wrapping(Into::<i16>::into(offset))).0 as u16;
+    }
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn bne_relative() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn bne_zero_set() {
+        let est = EmulationStateTest {
+            instructions: &[0xD0, 0xFF],
+            initial_cpu_state: CpuState {
+                ps: Some(PS::Z),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                pc: Some(0x02),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn bne_positive_offset() {
+        let est = EmulationStateTest {
+            instructions: &[0xD0, 0x01],
+            test_cpu_state: CpuState {
+                pc: Some(0x03),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn bne_negative_offset() {
+        let est = EmulationStateTest {
+            instructions: &[0xD0, 0xFF],
+            test_cpu_state: CpuState {
+                pc: Some(0x01),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
