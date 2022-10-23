7f3d95a8252928ec56ca5b8141ff872cff63e27f
Josh Forletta <joshforletta@gmail.com>
2022-10-23 13:27:57 -0700

    Implemented dex instruction

--- src/instruction/dex.rs
+++ src/instruction/dex.rs
@@ -1,8 +1,9 @@
-use std::error::Error;
+use std::{error::Error, num::Wrapping};
 
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,73 @@ pub const DEX_IMPLIED: Instruction = Instruction {
     call: dex,
 };
 
-pub fn dex(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn dex(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.x = (Wrapping(cpu.x) - Wrapping(1)).0;
+
+    cpu.ps.set(PS::Z, cpu.x == 0);
+    cpu.ps.set(PS::N, (cpu.x & 0x80) != 0);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn dex_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn dex_zero_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0xCA],
+            initial_cpu_state: CpuState {
+                x: Some(0x01),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::Z),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn dex_negative_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0xCA],
+            initial_cpu_state: CpuState {
+                x: Some(0x00),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::N),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn dex() {
+        let est = EmulationStateTest {
+            instructions: &[0xCA],
+            initial_cpu_state: CpuState {
+                x: Some(0x69),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                x: Some(0x68),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
