85bf80ff3a98853bb69fb236f8983617bc9b5fb4
Josh Forletta <joshforletta@gmail.com>
2022-10-23 11:56:24 -0700

    Implemented clc instruction

--- src/instruction/clc.rs
+++ src/instruction/clc.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,34 @@ pub const CLC_IMPLIED: Instruction = Instruction {
     call: clc,
 };
 
-pub fn clc(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn clc(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.ps.remove(PS::C);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn clc_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn clc() {
+        let est = EmulationStateTest {
+            instructions: &[0x18],
+            initial_cpu_state: CpuState {
+                ps: Some(PS::C),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::empty()),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
