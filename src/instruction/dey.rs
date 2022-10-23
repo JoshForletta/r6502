ea24ee49771a95de928733ff8072ed901407bc78
Josh Forletta <joshforletta@gmail.com>
2022-10-23 13:31:54 -0700

    Implemented dey instruction

--- src/instruction/dey.rs
+++ src/instruction/dey.rs
@@ -1,8 +1,9 @@
-use std::error::Error;
+use std::{error::Error, num::Wrapping};
 
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,73 @@ pub const DEY_IMPLIED: Instruction = Instruction {
     call: dey,
 };
 
-pub fn dey(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn dey(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.y = (Wrapping(cpu.y) - Wrapping(1)).0;
+
+    cpu.ps.set(PS::Z, cpu.y == 0);
+    cpu.ps.set(PS::N, (cpu.y & 0x80) != 0);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn dey_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn dey_zero_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x88],
+            initial_cpu_state: CpuState {
+                y: Some(0x01),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::Z),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn dey_negative_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x88],
+            initial_cpu_state: CpuState {
+                y: Some(0x00),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::N),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn dey() {
+        let est = EmulationStateTest {
+            instructions: &[0x88],
+            initial_cpu_state: CpuState {
+                y: Some(0x69),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                y: Some(0x68),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
