d371cde42f559898a0c7280de93176f4630e9915
Josh Forletta <joshforletta@gmail.com>
2022-10-28 14:27:20 -0700

    Implemented plp instruction

--- src/instruction/plp.rs
+++ src/instruction/plp.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,35 @@ pub const PLP_IMPLIED: Instruction = Instruction {
     call: plp,
 };
 
-pub fn plp(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn plp(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.ps = PS::from_bits(cpu.pull()?).unwrap_or(PS::empty());
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn plp_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn plp() {
+        let est = EmulationStateTest {
+            instructions: &[0x28],
+            initial_mem: &[(0x0100, (PS::C | PS::N).bits())],
+            initial_cpu_state: CpuState {
+                sp: Some(0x01),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::C | PS::N),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
