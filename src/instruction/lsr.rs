fb13d7c26057a6f0b78640cedbb8788d1bf12bc5
Josh Forletta <joshforletta@gmail.com>
2022-10-28 13:16:26 -0700

    Implemented lsr instruction

--- src/instruction/lsr.rs
+++ src/instruction/lsr.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -42,27 +43,70 @@ pub const LSR_ABSOLUTE_X: Instruction = Instruction {
 };
 
 pub fn lsr(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    let target = (am.call)(cpu)?;
+    let data = (*target as u16) << 7;
+
+    let (carry, data) = (data as u8, (data >> 8) as u8);
+
+    *target = data;
+
+    cpu.ps.set(PS::Z, data == 0);
+    cpu.ps.set(PS::C, carry != 0);
+    cpu.ps.set(PS::N, false);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn lsr_accumulator() {}
-    //
-    // #[test]
-    // fn lsr_zero_page() {}
-    //
-    // #[test]
-    // fn lsr_zero_page_x() {}
-    //
-    // #[test]
-    // fn lsr_absolute() {}
-    //
-    // #[test]
-    // fn lsr_absolute_x() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn lsr_carry_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x46, 0x02, 0xF1],
+            test_cpu_state: CpuState {
+                ps: Some(PS::C),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn lsr_zero_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x46, 0x02, 0x00],
+            test_cpu_state: CpuState {
+                ps: Some(PS::Z),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn lsr() {
+        let est = EmulationStateTest {
+            instructions: &[0x4A],
+            initial_cpu_state: CpuState {
+                a: Some(0xFF),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                a: Some(0x7F),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
