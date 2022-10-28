0add387a37a1e7fde29f9177d0f924f88b8469d0
Josh Forletta <joshforletta@gmail.com>
2022-10-28 12:36:49 -0700

    Implemented ldx instruction

--- src/instruction/ldx.rs
+++ src/instruction/ldx.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -42,27 +43,60 @@ pub const LDX_ABSOLUTE_Y: Instruction = Instruction {
 };
 
 pub fn ldx(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    cpu.x = *(am.call)(cpu)?;
+
+    cpu.ps.set(PS::Z, cpu.x == 0);
+    cpu.ps.set(PS::N, (cpu.x & 0x80) != 0);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn ldx_immediate() {}
-    //
-    // #[test]
-    // fn ldx_zero_page() {}
-    //
-    // #[test]
-    // fn ldx_zero_page_y() {}
-    //
-    // #[test]
-    // fn ldx_absolute() {}
-    //
-    // #[test]
-    // fn ldx_absolute_y() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn ldx_zero_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0xA2, 0x00],
+            test_cpu_state: CpuState {
+                ps: Some(PS::Z),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn ldx_negative_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0xA2, 0x80],
+            test_cpu_state: CpuState {
+                ps: Some(PS::N),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn ldx() {
+        let est = EmulationStateTest {
+            instructions: &[0xA2, 0x69],
+            test_cpu_state: CpuState {
+                x: Some(0x69),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
