564f5fd85bb15b673b0b409beedf2d3b18ec94dc
Josh Forletta <joshforletta@gmail.com>
2022-10-28 15:01:14 -0700

    Implemented ror instruction

--- src/instruction/ror.rs
+++ src/instruction/ror.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -42,27 +43,116 @@ pub const ROR_ABSOLUTE_X: Instruction = Instruction {
 };
 
 pub fn ror(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    let carry = cpu.ps.contains(PS::C);
+    let target = (am.call)(cpu)?;
+    let data = ((*target as u16) << 7) | ((carry as u16) << 15);
+
+    let (carry, data) = (data as u8, (data >> 8) as u8);
+
+    *target = data;
+
+    cpu.ps.set(PS::Z, data == 0);
+    cpu.ps.set(PS::C, carry != 0);
+    cpu.ps.set(PS::N, (data & 0x80) != 0);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn ror_accumulator() {}
-    //
-    // #[test]
-    // fn ror_zero_page() {}
-    //
-    // #[test]
-    // fn ror_zero_page_x() {}
-    //
-    // #[test]
-    // fn ror_absolute() {}
-    //
-    // #[test]
-    // fn ror_absolute_x() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn ror_carry_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x6A],
+            initial_cpu_state: CpuState {
+                a: Some(0xF1),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::C),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn ror_zero_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x6A],
+            initial_cpu_state: CpuState {
+                a: Some(0x00),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::Z),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn ror_negative_flag() {
+        let est = EmulationStateTest {
+            instructions: &[0x6A],
+            initial_cpu_state: CpuState {
+                ps: Some(PS::C),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                ps: Some(PS::N),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn ror() {
+        let est = EmulationStateTest {
+            instructions: &[0x6A],
+            initial_cpu_state: CpuState {
+                a: Some(0xF0),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                a: Some(0x78),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn ror_with_carry() {
+        let est = EmulationStateTest {
+            instructions: &[0x6A],
+            initial_cpu_state: CpuState {
+                ps: Some(PS::C),
+                a: Some(0xF0),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                a: Some(0xF8),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
