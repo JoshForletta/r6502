6dcf6b6923a726c318c40d8dbb899e05aa7351ab
Josh Forletta <joshforletta@gmail.com>
2022-10-28 12:21:45 -0700

    Implemented jsr instruction

--- src/instruction/jsr.rs
+++ src/instruction/jsr.rs
@@ -14,15 +14,31 @@ pub const JSR_ABSOLUTE: Instruction = Instruction {
 };
 
 pub fn jsr(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    (am.call)(cpu)?;
+
+    cpu.push_word(cpu.pc - 1)?;
+
+    cpu.pc = cpu.target_address;
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn jsr_absolute() {}
+    use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
+
+    #[test]
+    fn jsr() {
+        let est = EmulationStateTest {
+            instructions: &[0x20, 0x69, 0xFF],
+            test_cpu_state: CpuState {
+                pc: Some(0xFF69),
+                ..Default::default()
+            },
+            mem_tests: &[(0x0100, 0x02), (0x0101, 0x00)],
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
