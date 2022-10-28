9352d60468777a15c885477f15593ab0df77faeb
Josh Forletta <joshforletta@gmail.com>
2022-10-28 11:42:03 -0700

    Implemented jmp instruction

--- src/instruction/jmp.rs
+++ src/instruction/jmp.rs
@@ -21,18 +21,42 @@ pub const JMP_INDIRECT: Instruction = Instruction {
 };
 
 pub fn jmp(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+    (am.call)(cpu)?;
+
+    cpu.pc = cpu.target_address;
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn jmp_absolute() {}
-    //
-    // #[test]
-    // fn jmp_indirect() {}
+    use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
+
+    #[test]
+    fn jmp_absolute() {
+        let est = EmulationStateTest {
+            instructions: &[0x4C, 0x69, 0xFF],
+            test_cpu_state: CpuState {
+                pc: Some(0xFF69),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
+
+    #[test]
+    fn jmp_indirect() {
+        let est = EmulationStateTest {
+            instructions: &[0x6C, 0x03, 0x00, 0x69, 0xFF],
+            test_cpu_state: CpuState {
+                pc: Some(0xFF69),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
