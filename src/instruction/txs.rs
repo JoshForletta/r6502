225f05feb30fed530414246cd2f70ac4c297610e
Josh Forletta <joshforletta@gmail.com>
2022-10-30 13:30:02 -0700

    Implemented txs instruction

--- src/instruction/txs.rs
+++ src/instruction/txs.rs
@@ -13,16 +13,31 @@ pub const TXS_IMPLIED: Instruction = Instruction {
     call: txs,
 };
 
-pub fn txs(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn txs(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.sp = cpu.x;
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn txs_implied() {}
+    use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
+
+    #[test]
+    fn txs() {
+        let est = EmulationStateTest {
+            instructions: &[0x9A],
+            initial_cpu_state: CpuState {
+                x: Some(0x69),
+                ..Default::default()
+            },
+            test_cpu_state: CpuState {
+                sp: Some(0x69),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
