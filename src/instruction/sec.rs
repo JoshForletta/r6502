29511a8a8b36099b36a9a60cd37aaae7fb4f4e6f
Josh Forletta <joshforletta@gmail.com>
2022-10-30 13:02:55 -0700

    Implemented sec instruction

--- src/instruction/sec.rs
+++ src/instruction/sec.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,30 @@ pub const SEC_IMPLIED: Instruction = Instruction {
     call: sec,
 };
 
-pub fn sec(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn sec(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.ps.set(PS::C, true);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn sec_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn sec() {
+        let est = EmulationStateTest {
+            instructions: &[0x38],
+            test_cpu_state: CpuState {
+                ps: Some(PS::C),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
