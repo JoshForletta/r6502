079829f1858c486348b2120f8a0db87ee827705e
Josh Forletta <joshforletta@gmail.com>
2022-10-30 13:05:09 -0700

    Implemented sed instruction

--- src/instruction/sed.rs
+++ src/instruction/sed.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,30 @@ pub const SED_IMPLIED: Instruction = Instruction {
     call: sed,
 };
 
-pub fn sed(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn sed(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.ps.set(PS::D, true);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn sed_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn sed() {
+        let est = EmulationStateTest {
+            instructions: &[0xF8],
+            test_cpu_state: CpuState {
+                ps: Some(PS::D),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
