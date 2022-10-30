e0a1222a29e8c2e5ac52440f86a44a15f383bd19
Josh Forletta <joshforletta@gmail.com>
2022-10-30 13:07:07 -0700

    Implemented sei instruction

--- src/instruction/sei.rs
+++ src/instruction/sei.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use crate::{
     addressing_mode::{self, AddressingMode},
     instruction::Instruction,
+    r6502::PS,
     R6502,
 };
 
@@ -13,16 +14,30 @@ pub const SEI_IMPLIED: Instruction = Instruction {
     call: sei,
 };
 
-pub fn sei(cpu: &mut R6502, am: AddressingMode) -> Result<(), Box<dyn Error>> {
-    let _target = (am.call)(cpu)?;
+pub fn sei(cpu: &mut R6502, _am: AddressingMode) -> Result<(), Box<dyn Error>> {
+    cpu.ps.set(PS::I, true);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    // use crate::test_utils::{test_emulation_state, CpuState, EmulationStateTest};
-    //
-    // #[test]
-    // fn sei_implied() {}
+    use crate::{
+        r6502::PS,
+        test_utils::{test_emulation_state, CpuState, EmulationStateTest},
+    };
+
+    #[test]
+    fn sei() {
+        let est = EmulationStateTest {
+            instructions: &[0x78],
+            test_cpu_state: CpuState {
+                ps: Some(PS::I),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        test_emulation_state(&est);
+    }
 }
