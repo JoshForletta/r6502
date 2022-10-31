5d321b0c20bcc7e6ca74f57b18e28005dbcb73a6
Josh Forletta <joshforletta@gmail.com>
2022-10-30 17:41:23 -0700

    Added basic binary file execution

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
+use std::{
+    fs::{self, File},
+    io::stdin,
+    path::PathBuf,
+    thread::sleep,
+    time::Duration,
+};
+
+use byteorder::ReadBytesExt;
+use clap::Parser;
+
 use r6502::{
-    device::{Device, Ram},
+    device::{Device, Output, Ram},
     R6502,
 };
 
+#[derive(Parser)]
+struct Args {
+    // path to program
+    bin: PathBuf,
+}
+
 fn main() {
-    // let mut cpu: R6502 = R6502::new();
-    // let mut ram: Ram = Ram::with_address_range(0xFF00, 0xFFFF);
-    //
-    // ram.set(0xfffc, 0x00).unwrap();
-    // ram.set(0xfffd, 0xff).unwrap();
-    // ram.set(0xff00, 0x01).unwrap(); // temp LDA IMM ins
-    // ram.set(0xff01, 0x01).unwrap();
-    // // ram.set(0xff02, 0xff).unwrap();
-    //
-    // cpu.mount_device(Box::new(ram));
-    //
-    // cpu.reset();
-    //
-    // // dbg!(&cpu);
-    //
-    // cpu.clock().unwrap();
-    //
-    // dbg!(cpu);
+    let args = Args::parse();
+
+    let mut cpu: R6502 = R6502::new();
+    let mut ram: Ram = Ram::with_address_range(0x0000, 0xFFFF);
+    let output = Output::with_address(0xFFFB);
+
+    ram.set(0xFFFC, 0x00).unwrap();
+    ram.set(0xFFFD, 0x00).unwrap();
+
+    let mut bin = File::open(&args.bin).expect("Failed to open file");
+
+    for addr in 0..fs::metadata(args.bin).unwrap().len() {
+        ram.set(addr as u16, bin.read_u8().unwrap())
+            .expect("Failed to load program");
+    }
+
+    cpu.mount_device(Box::new(ram));
+    cpu.mount_device(Box::new(output));
+
+    let mut buf = String::new();
+
+    loop {
+        cpu.clock().unwrap();
+
+        stdin().read_line(&mut buf).unwrap();
+    }
 }
