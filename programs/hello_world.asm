7c16dc1148732426c5fa36dd67e42e8f1eba18cc
Josh Forletta <joshforletta@gmail.com>
2022-11-05 22:20:57 -0700

    Moved 6502 assembly to this directory

---
+++ programs/hello_world.asm
@@ -0,0 +1,32 @@
+    lda #$68
+    sta $FFFB
+
+    lda #$65
+    sta $FFFB
+
+    lda #$6c
+    sta $FFFB
+
+    lda #$6c
+    sta $FFFB
+
+    lda #$6f
+    sta $FFFB
+
+    lda #$20
+    sta $FFFB
+
+    lda #$77
+    sta $FFFB
+
+    lda #$6f
+    sta $FFFB
+
+    lda #$72
+    sta $FFFB
+
+    lda #$6c
+    sta $FFFB
+
+    lda #$64
+    sta $FFFB
